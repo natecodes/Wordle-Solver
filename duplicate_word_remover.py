b0da86ffcd3d04c541561313e32efdd9b31ac9ab
Nathan <nathanparkengineering@gmail.com>
2022-02-24 00:24:14 -0800

    updated script to sort words after removing duplicates

--- duplicate_word_remover.py
+++ duplicate_word_remover.py
@@ -4,7 +4,8 @@ fname = input("What is the file name (include.txt)?: ")
 
 with open(fname, "r") as fin, open(fname[:-3] + "_removed_duplicates", "w") as fout:
     for line in fin:
-        h = hash(line)
-        if h not in seen:
-            fout.write(line)
-            seen.add(h)
+        # h = hash(line)
+        if line not in seen:
+            # fout.write(line)
+            seen.add(line)
+    fout.writelines(sorted(seen))
