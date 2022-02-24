09a35ff9da488730175cbe8761e98d558fa3cbe5
Nathan <nathanparkengineering@gmail.com>
2022-02-24 00:25:31 -0800

    fixed script to append .txt extension to new file

--- duplicate_word_remover.py
+++ duplicate_word_remover.py
@@ -2,7 +2,7 @@ seen = set()
 
 fname = input("What is the file name (include.txt)?: ")
 
-with open(fname, "r") as fin, open(fname[:-3] + "_removed_duplicates", "w") as fout:
+with open(fname, "r") as fin, open(fname[:-4] + "_removed_duplicates.txt", "w") as fout:
     for line in fin:
         # h = hash(line)
         if line not in seen:
